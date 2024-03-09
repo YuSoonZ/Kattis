88d447e73964735f34babc6ac4348c45636d2d50
HENRY\YS <yusoonng3@gmail.com>
2024-03-10 00:27:29 +0800

    Update bokforing code

--- src/main/java/bokforing.java
+++ src/main/java/bokforing.java
@@ -1,5 +1,6 @@
 import java.util.;
 import java.io.;
+
 public class bokforing {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
